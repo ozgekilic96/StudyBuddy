0c427c8cdf05b18ab701606561ac1cc4932e043a
Ulrich Thofehrn <welfito@icloud.com>
2023-08-31 14:11:35 +0200

    updated sessions controller file

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,48 @@
 class SessionsController < ApplicationController
   def new
+    @session = Session.new
   end
 
   def create
+    @session = Session.new(session_params)
+    if @session.save
+      redirect_to session_path
+    else
+      render :new
+    end
   end
 
   def show
+    @session = Session.find(params[:id])
   end
 
   def update
+    @session = current_user.session.find(params[:id])
+
+    if @session.update(session_params)
+      redirect_to @session, notice: 'Session was successfully updated'
+    else
+      render :edit
+    end
   end
 
   def edit
+    @session = Session.find(params[:id])
+    if @session.user != current_user
+      redirect_to @session, alert: 'You do not have permission to edit this session'
+    end
+  end
+
+  def join
+    @session = Session.find(params[:id])
+    @membership = Membership.new(session: @session, user: current_user)
+    @membership.save
+    redirect_to @session, notice: 'You have joined the session.'
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:name, :description, :address)
   end
 end
