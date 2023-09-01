491fcc0578c0a963545c2daa6bc9cb13a43c5fc4
Giovane Nery <giovane.nery@hotmail.com>
2023-09-02 00:01:09 +0200

    Fixed Sign Up to Subject Page

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,6 +4,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
+  protected
+
+  def after_sign_up_path_for(resource)
+    profilesubjects_path
+  end
+
   # GET /resource/sign_up
   # def new
   #   super
