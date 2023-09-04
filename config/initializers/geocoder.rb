34e12bda35f6b21c85b1820017ca4ca03df150e5
Giovane Nery <giovane.nery@hotmail.com>
2023-09-04 11:15:41 +0200

    Map fixed

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -2,14 +2,17 @@ Geocoder.configure(
   # Geocoding options
   # timeout: 3,                 # geocoding service timeout (secs)
   # lookup: :nominatim,         # name of geocoding service (symbol)
+  lookup: :geoapify,
+  api_key: "ac1263e0ae2d4e0ba31911c3fa8ba485",
   # ip_lookup: :ipinfo_io,      # name of IP address geocoding service (symbol)
   # language: :en,              # ISO-639 language code
   # use_https: false,           # use HTTPS for lookup requests? (if supported)
   # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
   # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
-  # api_key: nil,               # API key for geocoding service
-  # cache: nil,                 # cache object (must respond to #[], #[]=, and #del)
-  units: :km, # Defaults to miles (:mi)
+  # API key for geocoding service
+  # cache: nil,
+  # cache object (must respond to #[], #[]=, and #del)
+  units: :km # Defaults to miles (:mi)
   # Exceptions that should not be rescued by default
   # (if you want to implement custom error handling);
   # supports SocketError and Timeout::Error
