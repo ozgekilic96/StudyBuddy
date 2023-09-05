841d915b59d20748b3eb8c51abd342463827d659
özge kilic <ozgeklc096@gmail.com>
2023-09-05 17:46:25 +0300

    end for today

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -17,6 +17,6 @@ class CommentsController < ApplicationController
   end
 
   def comment_params
-    params.require(:comment).permit(:content)
+    params.require(:comment).permit(:content )
   end
 end
